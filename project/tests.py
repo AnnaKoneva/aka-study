22b67a1838cf908e4e9b910e118515f5e9a4e1a0
big <NNChutchikov@gmail.com>
2011-04-17 02:52:27 +0800

    use pep8, pychecker and  pylint to fix mistakes

--- project/tests.py
+++ project/tests.py
@@ -1,8 +1,9 @@
-from serv import HTTPServer,  parse_http, encode_http, Request
+from serv import HTTPServer, parse_http, encode_http, Request
 from handlers import serve_static
 from nose.tools import eq_, raises
 from coverage import coverage
 from cStringIO import StringIO
+
 
 class MockConnection:
 
@@ -23,69 +24,58 @@ class MockConnection:
     def close(self):
         pass
 
+
 class MockClient:
+
     def __init__(self, server):
         self.server = server
-        
 
-    def __call__ (self, method, url, body=None, **headers):
+    def __call__(self, method, url, body=None, **headers):
         conn = MockConnection(encode_http((method, url, "HTTP/1.0"), body, **headers))
         self.server.on_connect(conn, None)
 
         return parse_http(conn.sent_data)
- 
 
 
 class TestHTTPServer:
-   
+
     def setup(self):
         self.server = HTTPServer(11112, debug=True)
         self.client = MockClient(self.server)
 
     def test_on_connect(self):
-      
-       
-        
-        handler = lambda request: request.reply('<H1>"Hello world!!!!"</H1>',content_type = 'text/html')
-        pattern = lambda request: request.url =="/html/"
+        handler = lambda request: request.reply('<H1>"Hello world!!!!"</H1>', content_type='text/html')
+        pattern = lambda request: request.url == "/html/"
         self.server.register(pattern, handler)
-	
 
         reply, headers, body = self.client('GET', '/html/')
         eq_(reply, ['HTTP/1.0', '200', 'OK'])
         eq_(headers['CONTENT-TYPE'], 'text/html')
 
     def test_change_handlers(self):
-      
-        
+
         def pattern(request):
-             if request.url == '/html/':
+            if request.url == '/html/':
                 request.test = 'hell world'
                 return True
 
         self.server.register(pattern, lambda request: request.reply(request.test, content_type='text/html'))
 
         reply, headers, body = self.client('GET', '/html/')
-       
+
         eq_(body, 'hell world')
 
-
     def test_multihandlers(self):
+        self.server.register(lambda request: request.method == "POST", lambda request: request.reply('post'))
+        self.server.register(lambda request: request.url == '/test/', lambda request: request.reply('other'))
 
- 
-        self.server.register(lambda request: request.method == "POST", lambda request: request.reply('post')) 
-        self.server.register(lambda request: request.url == '/test/', lambda request: request.reply('other'))  
-
-        reply, headers, body = self.client ('POST', '/test/')
+        reply, headers, body = self.client('POST', '/test/')
         eq_('post', body)
 
         reply, headers, body = self.client('GET', '/test/')
         eq_('other', body)
 
-
     def test_multiline_body(self):
-
- 
         self.server.register(
             lambda request: request.url == '/multiline',
             lambda request: request.reply('<strong>multi-\r\nline\r\nbody</strong>', content_type='text/html')
@@ -97,45 +87,46 @@ class TestHTTPServer:
     def test_handlers(self):
 
         self.server.register(lambda r: r.url == True, lambda r: None)
-	request, headers, body = self.client('GET', '/')
-	eq_(request[1], '404')
+        request, headers, body = self.client('GET', '/')
+        eq_(request[1], '404')
         eq_(body, '<h1>404 Not Found</h1>')
 
         self.server.register(lambda r: r.url == '/str', lambda r: 'This is a string reply')
-	request, headers, body = self.client('GET', '/str')
-	eq_(request[1], '200')
-	eq_(body, 'This is a string reply')
+        request, headers, body = self.client('GET', '/str')
+        eq_(request[1], '200')
+        eq_(body, 'This is a string reply')
 
-	self.server.register(lambda r: r.url == 'utf', lambda r: 'This is a utf-8 reply'.decode('utf-8'))
-	request, headers, body = self.client('GET', 'utf')
-	eq_(request[1], '200')
-	eq_(body, 'This is a utf-8 reply')
+        self.server.register(lambda r: r.url == 'utf', lambda r: 'This is a utf-8 reply'.decode('utf-8'))
+        request, headers, body = self.client('GET', 'utf')
+        eq_(request[1], '200')
+        eq_(body, 'This is a utf-8 reply')
 
-	self.server.register(lambda r: r.url == '/iter', lambda r: ['asd', 'fgh'])
-	request, headers, body = self.client('GET', '/iter')
-	eq_(request[1], '200')
-	eq_(body, 'asdfgh')
+        self.server.register(lambda r: r.url == '/iter', lambda r: ['asd', 'fgh'])
+        request, headers, body = self.client('GET', '/iter')
+        eq_(request[1], '200')
+        eq_(body, 'asdfgh')
 
-	self.server.register(lambda r: r.url == '/file', lambda r: open('handlers.py').read(9))
-	request, headers, body = self.client('GET', '/file')
-	eq_(request[1], '200')
-	eq_(body, 'import os')
+        self.server.register(lambda r: r.url == '/file', lambda r: open('handlers.py').read(9))
+        request, headers, body = self.client('GET', '/file')
+        eq_(request[1], '200')
+        eq_(body, 'import os')
 
         self.server.options["debug"] = False
-	self.server.register(lambda r: r.url == '/crash/me/', lambda r: no_you)
+        self.server.register(lambda r: r.url == '/crash/me/', lambda r: no_you)
         request, headers, body = self.client('GET', '/crash/me/')
-        eq_(request[1], '500')  
+        eq_(request[1], '500')
         eq_(request[2], 'Internal Server Error')
-    
+
 
 class TestHandlers(object):
+
     def setup(self):
 
         self.server = HTTPServer(debug=True)
         self.client = MockClient(self.server)
 
     def test_static(self):
-         
+
         self.server.register(*serve_static(address='/', root="."))
         reply, headers, body = self.client('GET', '/handlers.py')
         data = open('handlers.py').read()
@@ -148,12 +139,11 @@ class TestHandlers(object):
 
 
 class TestHTTPRequest:
-    
-    def test_constructor(self):pass
 
-    
+    def test_constructor(self):
+        pass
+
 
 if __name__ == "__main__":
     import nose
     nose.run()
-
